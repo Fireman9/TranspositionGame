ce3bd957ef08a8ca7849dcacf2b96adf9a962e5c
CyberFrog <>
2020-11-09 01:27:58 +0200

    Added decision tree

---
+++ DecisionTree.cpp
@@ -0,0 +1,6 @@
+#include "DecisionTree.h"
+
+DecisionTree::DecisionTree()
+{
+
+}
---
+++ DecisionTree.h
@@ -0,0 +1,11 @@
+#ifndef DECISIONTREE_H
+#define DECISIONTREE_H
+
+
+class DecisionTree
+{
+public:
+    DecisionTree();
+};
+
+#endif // DECISIONTREE_H
