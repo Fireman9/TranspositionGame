8458286fb08ef5df0f180860c741a5ab8ee6f89d
CyberFrog <>
2020-11-09 01:28:26 +0200

    Decision tree realized

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -1,6 +1,19 @@
 #include "DecisionTree.h"
 
-DecisionTree::DecisionTree()
+DecisionTree::DecisionTree(QVector<int> cardPosCopy, int depth, int a, int b)
 {
-
+    this->a = a;
+    this->b = b;
+    this->terminal = false;
+    this->cards = cardPosCopy;
+    this->depth = depth;
+    if(this->depth < 3){
+        for(int i = 0; i < 7; i++){
+            if(cards[i] < cards[i + 1]){
+                QVector<int> temp = cards;
+                std::swap(temp[i], temp[i + 1]);
+                childs.push_back(new DecisionTree(temp, this->depth + 1, temp[i + 1], temp[i]));
+            }
+        }
+    }
 }
--- DecisionTree.h
+++ DecisionTree.h
@@ -1,11 +1,18 @@
 #ifndef DECISIONTREE_H
 #define DECISIONTREE_H
 
+#include <QVector>
 
 class DecisionTree
 {
 public:
-    DecisionTree();
+    DecisionTree(QVector<int> cardPosCopy, int depth, int a, int b);
+    QVector<int> cards;
+    QVector<DecisionTree*> childs;
+    int depth;
+    int count;
+    int a, b;
+    bool terminal;
 };
 
 #endif // DECISIONTREE_H
